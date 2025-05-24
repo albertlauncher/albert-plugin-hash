425b8cb69ceab89c5d3a73cc82911074157dcf92
Manuel Schneider <manuelschneid3r@gmail.com>
2025-05-24 15:42:31 +0200

    Avoid runtime conversion from utf8 for QString construction

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -1,26 +1,26 @@
-// Copyright (c) 2022-2024 Manuel Schneider
+// Copyright (c) 2022-2025 Manuel Schneider
 
-#include <albert/albert.h>
-#include <albert/standarditem.h>
 #include "plugin.h"
 #include <QCoreApplication>
 #include <QCryptographicHash>
 #include <QMetaEnum>
+#include <albert/albert.h>
+#include <albert/standarditem.h>
 #include <memory>
+using namespace albert::util;
 using namespace albert;
 using namespace std;
-using namespace util;
 
 static int algo_count = QMetaEnum::fromType<QCryptographicHash::Algorithm>().keyCount()-1;
 
 static shared_ptr<Item> buildItem(int algo_index, const QString& string_to_hash)
 {
-    QString algo_name = QMetaEnum::fromType<QCryptographicHash::Algorithm>().key(algo_index);
+    auto algo_name = QString::fromLatin1(QMetaEnum::fromType<QCryptographicHash::Algorithm>().key(algo_index));
     int algo_enum_value = QMetaEnum::fromType<QCryptographicHash::Algorithm>().value(algo_index);
 
     QCryptographicHash hash(static_cast<QCryptographicHash::Algorithm>(algo_enum_value));
     hash.addData(string_to_hash.toUtf8());
-    QByteArray hashString = hash.result().toHex();
+    auto hashString = QString::fromLatin1(hash.result().toHex());
 
     static const auto tr_c = Plugin::tr("Copy");
     static const auto tr_cs = Plugin::tr("Copy short form (8 char)");
@@ -29,25 +29,29 @@ static shared_ptr<Item> buildItem(int algo_index, const QString& string_to_hash)
         algo_name,
         hashString,
         algo_name,
-        QStringList({":hash"}),
+        QStringList({QStringLiteral(":hash")}),
         {
             {
-                "c", tr_c,
-                [hashString](){ albert::setClipboardText(QString(hashString)); }
+                QStringLiteral("c"), tr_c,
+                [hashString]{ albert::setClipboardText(QString(hashString)); }
             },
             {
-                "cs", tr_cs,
-                [hashString](){ albert::setClipboardText(QString(hashString.left(8))); }
+                QStringLiteral("cs"), tr_cs,
+                [hashString]{ albert::setClipboardText(QString(hashString.left(8))); }
             }
         }
     );
-};
+}
 
 vector<RankItem> Plugin::handleGlobalQuery(const Query &query)
 {
     vector<RankItem> results;
-    for (int i = 0; i < algo_count; ++i){
-        auto prefix = QString("%1 ").arg(QMetaEnum::fromType<QCryptographicHash::Algorithm>().key(i)).toLower();
+    for (int i = 0; i < algo_count; ++i)
+    {
+        const auto u8_algo_name = QMetaEnum::fromType<QCryptographicHash::Algorithm>().key(i);
+        const auto algo_name = QString::fromUtf8(u8_algo_name);
+        auto prefix = algo_name.toLower() + u' ';
+
         if (query.string().size() >= prefix.size() && query.string().startsWith(prefix, Qt::CaseInsensitive)) {
             QString string_to_hash = query.string().mid(prefix.size());
             results.emplace_back(buildItem(i, string_to_hash), 1.0f);
